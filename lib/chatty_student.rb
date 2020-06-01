0e8bb5bb71ff65ea8cc48ad63131cdf40d596c60
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 18:23:54 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,11 @@
 class Chatty_student < Student
   def hello
-    super
+    Super
     @hello_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   def raise_hand
-    super
+    Super
     @pick_me = "Pick me!"
   end
 
